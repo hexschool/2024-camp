02b5581db5866292dd17d2459e3929c8739f9ed4
Jay <kred110396@gmail.com>
2024-03-19 18:20:17 +0800

    feat: add work2 section for home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -41,10 +41,10 @@ import AppHeader from '@/components/AppHeader.vue';
     >
       <div class="container">
         <div class="row">
-          <div class="col-12 col-md-6">
+          <div class="col-12 col-md-6 col-xl-7">
             <img class="img-fluid" src="@/assets/images/homepage-work1.png" alt="work1">
           </div>
-          <div class="col-12 col-md-6 d-none d-md-flex flex-md-column justify-content-center">
+          <div class="col-12 col-md-6 col-xl-5 d-none d-md-flex flex-md-column justify-content-center">
             <div class="px-7 py-9 bg-white-overlay rounded-5">
               <h3 class="mb-2 text-primary-50">星際旅行訂票平台</h3>
               <p class="mb-3 fs-6 text-primary-50">悠遊宇宙的夢想，從這裡開始實現</p>
@@ -71,9 +71,65 @@ import AppHeader from '@/components/AppHeader.vue';
       <h3 class="mb-2">星際旅行訂票平台</h3>
       <p class="mb-3 fs-6 text-primary-600">悠遊宇宙的夢想，從這裡開始實現</p>
       <div class="mb-4">
-        <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">網頁設計</span>
-        <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">響應式設計</span>
+        <span class="badge mb-3 me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">網頁設計</span>
+        <span class="badge mb-3 me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">響應式設計</span>
         <span class="badge fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">Bootstrap</span>
+      </div>
+      <a
+        class="btn btn-sm px-3 py-2 fs-6 fw-bold text-primary-50 bg-primary-900"
+        href="#"
+      >
+        完整介紹
+        <span class="material-symbols-outlined align-top">arrow_forward</span>
+      </a>
+    </div>
+
+    <section
+      class="position-relative pt-9"
+      style="background-image: url('/src/assets/images/homepage-work2-lg.png');background-repeat: no-repeat;background-position: center;
+      background-size: cover;
+      min-height: 390px;"
+    >
+      <div class="container">
+        <div class="row">
+          <div
+            class="d-none d-md-block col-md-6 position-absolute top-0 start-0 h-100"
+            style="background-color: #0A101380;"
+          >
+          </div>
+          <div
+            class="col-12 col-md-6 col-xl-5 d-none d-md-flex flex-md-column justify-content-center"
+            
+          >
+            <div class="px-7 py-9 bg-white-overlay rounded-5">
+              <h3 class="mb-2 text-primary-50">理財APP</h3>
+              <p class="mb-3 fs-6 text-primary-50">連動帳戶與行動支付，讓 AI 提供您最好的理財建議</p>
+              <div class="pb-7 mb-7 border-bottom border-white">
+                <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">APP設計</span>
+                <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">iOS</span>
+                <span class="badge fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">React</span>
+              </div>
+              <div class="d-flex justify-content-center">
+                <a
+                  class="btn btn-lg px-6 py-3 fs-5 fw-bold text-primary-50 bg-primary-900"
+                  href="#"
+                >
+                  完整介紹
+                  <span class="material-symbols-outlined align-text-bottom">arrow_forward</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <div class="d-md-none pt-4 pb-8 text-center">
+      <h3 class="mb-2">理財APP</h3>
+      <p class="mb-3 fs-6 text-primary-600">連動帳戶與行動支付，讓 AI 提供您最好的理財建議</p>
+      <div class="mb-4">
+        <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">APP設計</span>
+        <span class="badge me-2 fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">iOS</span>
+        <span class="badge fs-6 lh-base fw-normal text-primary-700 bg-primary-200 rounded-4">React</span>
       </div>
       <a
         class="btn btn-sm px-3 py-2 fs-6 fw-bold text-primary-50 bg-primary-900"
