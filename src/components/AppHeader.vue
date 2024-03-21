befac5bc3ea13f796a49c219ee4801a2da80554d
Jay <kred110396@gmail.com>
2024-03-21 23:23:42 +0800

    feat: add active style on Navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,19 +11,19 @@
         <div class="collapse navbar-collapse offset-md-3" id="navbarToggler">
           <ul class="navbar-nav me-auto mb-2 mb-md-0 text-center">
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link active" aria-current="page" href="#">首頁</a>
+              <a class="nav-link position-relative py-1 active" aria-current="page" href="#">首頁</a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link" href="#">作品集</a>
+              <a class="nav-link position-relative py-1" href="#">作品集</a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link" href="#">服務項目</a>
+              <a class="nav-link position-relative py-1" href="#">服務項目</a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link" href="#">部落格</a>
+              <a class="nav-link position-relative py-1" href="#">部落格</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">聯絡我</a>
+              <a class="nav-link position-relative py-1" href="#">聯絡我</a>
             </li>
           </ul>
         </div>
