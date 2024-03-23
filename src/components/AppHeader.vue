c8c0871af00170ba2e7cb3953f3f4cd18156aa96
Jay <kred110396@gmail.com>
2024-03-24 00:46:45 +0800

    fix: update navbar link active style on mobile view

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,24 +6,34 @@
           <img src="@/assets/images/logo.png" alt="logo">
         </a>
         <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="material-symbols-outlined fs-1">menu</span>
+          <span class="material-symbols-outlined fs-1 text-primary-900">menu</span>
         </button>
-        <div class="collapse navbar-collapse offset-md-3" id="navbarToggler">
+        <div class="collapse navbar-collapse offset-md-2 offset-lg-3" id="navbarToggler">
           <ul class="navbar-nav me-auto mb-2 mb-md-0 text-center">
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link position-relative py-1 active" aria-current="page" href="#">首頁</a>
+              <a class="nav-link active" aria-current="page" href="#">
+                <span class="position-relative px-2 py-1">首頁</span>
+              </a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link position-relative py-1" href="#">作品集</a>
+              <a class="nav-link" href="#">
+                <span class="position-relative px-2 py-1">作品集</span>
+              </a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link position-relative py-1" href="#">服務項目</a>
+              <a class="nav-link" href="#">
+                <span class="position-relative px-2 py-1">服務項目</span>
+              </a>
             </li>
             <li class="nav-item mb-4 mb-md-0 me-md-3 me-lg-7">
-              <a class="nav-link position-relative py-1" href="#">部落格</a>
+              <a class="nav-link" href="#">
+                <span class="position-relative px-2 py-1">部落格</span>
+              </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link position-relative py-1" href="#">聯絡我</a>
+              <a class="nav-link" href="#">
+                <span class="position-relative px-2 py-1">聯絡我</span>
+              </a>
             </li>
           </ul>
         </div>
